5681dd9c28c11aa5b60cd40c086c4e13edfe712a
Jessica Scheick <jbscheick@gmail.com>
2021-12-20 16:06:15 -0500

    add yaml include directive and use to generalize team

--- scripts/yaml2json.py
+++ scripts/yaml2json.py
@@ -2,13 +2,17 @@
 # usage: python <yaml_input_filename> <json_output_filename>
 # called by deploy_website.sh script
 
-import yaml
 import json
 import sys
+import yaml
+
+from yaml_tools import Loader
 
 yaml_file = sys.argv[1]
 json_file = sys.argv[2]
 
+Loader.add_constructor('!include', Loader.include)
+
 with open(yaml_file, 'r') as yaml_in, open(json_file, "w") as json_out:
-    yaml_object = yaml.safe_load(yaml_in) 
+    yaml_object = yaml.load(yaml_in, Loader=Loader) 
     json.dump(yaml_object, json_out)
